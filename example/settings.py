8150fdfd9ec1477f06695343e9090c88c6b53112
oliveirabrunoa <oliveirabrunoa@gmail.com>
2018-05-28 16:57:46 -0300

    Criação de arquivos de configuração heroku

--- example/settings.py
+++ example/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
